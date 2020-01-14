317ebde915cdd2e56b9cbc784fa28130794c8a94
Mac <aidencheung@foxmail.com>
2020-01-14 16:26:56 +0800

    p1 test

--- code/1-100/1/p1_test.go
+++ code/1-100/1/p1_test.go
@@ -5,22 +5,26 @@ import (
 	"testing"
 )
 func testTwoSum(t *testing.T){
-	type test struct {
+	type input struct {
 		input1 []int
 		input2 int
+	}
+	type test struct {
+		param input
 		want []int
 	}
 
-	tests := map[string]test{
-		{input1: []int{2, 7, 11, 15}, input2: 9, want []int{0, 1}},
+	tests := []test{
+		{param: input{input1: []int{3, 2, 4}, input2: 6}, want: []int{1, 2}},
+		{param: input{input1: []int{3, 2, 4}, input2: 5}, want: []int{0, 1}},
+		{param: input{input1: []int{0, 8, 7, 3, 3, 4, 2}, input2: 11}, want: []int{1, 3}},
+		{param: input{input1: []int{0, 1}, input2: 1}, want: []int{0, 1}},
 	}
 
 	for _, v := range tests {
-		result = twoSum(v.input1, v.input2)
+		result := twoSum(v.param.input1, v.param.input2)
 		if !reflect.DeepEqual(v.want, result) {
 			t.Errorf("want: %v, result: %v", v.want, result)
 		}
 	}
-
-
 }
